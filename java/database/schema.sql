6733d506b6f477c7d98d87f97cd6c5570732c2e5
Riggolus <nknappen@gmail.com>
2024-11-10 14:03:52 -0500

    Updated Schema

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -1,6 +1,6 @@
 START TRANSACTION;
 
-DROP TABLE IF EXISTS users_artist, artist, joke, users CASCADE;
+DROP TABLE IF EXISTS users CASCADE;
 
 CREATE TABLE users (
 	user_id SERIAL,
@@ -10,29 +10,6 @@ CREATE TABLE users (
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
 );
 
-CREATE TABLE joke (
-    joke_id serial PRIMARY KEY,
-    joke varchar(200),
-    user_id int,
---    setting up the fk constraint to the users table
---    user_id in joke table relates to the id in the users table
-    CONSTRAINT FK_users FOREIGN KEY (user_id) REFERENCES users (user_id)
-);
-
-CREATE TABLE artist (
-	artist_id serial PRIMARY KEY,
-	artist_name varchar(200),
-	artist_spotify_id varchar(200),
-	followers int
-);
-
-CREATE TABLE users_artist (
-	user_id int,
-	artist_id int,
-	CONSTRAINT PK_users_artist PRIMARY KEY (user_id, artist_id),
-	CONSTRAINT FK_users FOREIGN KEY (user_id) REFERENCES users (user_id),
-	CONSTRAINT FK_artist FOREIGN KEY (artist_id) REFERENCES artist(artist_id)
-);
 
 COMMIT TRANSACTION;
 
--- java/database/user.sql
+++ java/database/user.sql
@@ -2,25 +2,25 @@
 -- This script creates the database users and grants them the necessary permissions
 -- ********************************************************************************
 
-CREATE USER spotowner1
+CREATE USER spotowner2
 WITH PASSWORD 'finalcapstone';
 
 GRANT ALL
 ON ALL TABLES IN SCHEMA public
-TO spotowner1;
+TO spotowner2;
 
 GRANT ALL
 ON ALL SEQUENCES IN SCHEMA public
-TO spotowner1;
+TO spotowner2;
 
-CREATE USER i_see_spot
-WITH PASSWORD 'go-spot-go';
+CREATE USER frank_dog
+WITH PASSWORD 'go-frank-go';
 
 GRANT SELECT, INSERT, UPDATE, DELETE
 ON ALL TABLES IN SCHEMA public
-TO i_see_spot;
+TO frank_dog;
 
 GRANT USAGE, SELECT
 ON ALL SEQUENCES IN SCHEMA public
-TO i_see_spot;
+TO frank_dog;
 ;
