92b6e35947e395406fe581ebd84cf121e0a36efc
Riggolus <nknappen@gmail.com>
2024-11-10 18:53:54 -0500

    worked on basic layout of dashboard

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,15 +1,44 @@
 <template>
   <div id="capstone-app">
-    <div id="nav">
-      <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+    <div id="header">
+      <h1 id="title">Personal Finance Manager</h1>
+      <div id="nav">
+        <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
+        <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link>
+      </div>
     </div>
     <router-view />
   </div>
 </template>
 
-<style>
+<style scoped>
 #capstone-app {
-    text-align:center;
+  display: grid;
+  grid-template-columns: 100px auto;
+  grid-template-rows: 60px auto;
+  grid-template-areas:
+    "header header"
+    "sidebar main-content";
+  height: 100vh;
+  width: 100vw;
+  background: rgb(161, 178, 219);
 }
+
+#header {
+  grid-area: header;
+  background-color: #f8f9fa;
+  border-bottom: 1px solid #e9ecef;
+  padding: 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+#title {
+  margin: 0;
+  font-size: 1.5rem;
+  color: #000;
+}
+
+
 </style>
--- vue/src/views/DashboardView.vue
+++ vue/src/views/DashboardView.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
-    <p>You must be authenticated to see this</p>
+  <div id="dashboard">
+    <div class="home">
+      <h1>Home</h1>
+      <p>You must be authenticated to see this</p>
+    </div>
+
+    <sidebar-component/>
   </div>
-
-  <sidebar-component/>
-
 </template>
 
 <script>
@@ -19,10 +20,24 @@ export default {
 </script>
 
 <style>
+#dashboard {
+  display: grid;
+  grid-template-columns: 100px auto;
+  grid-template-rows: 60px auto;
+  grid-template-areas:
+    "header header"
+    "sidebar main-content";
+  height: 100vh;
+  width: 100vw;
+  background: rgb(161, 178, 219);
+}
+
 .home {
+  grid-area: main-content;
   padding: 20px;
 }
 sidebar-component {
+  grid-area: sidebar;
   background-color: #f8f9fa;
   border-right: 1px solid #e9ecef;
   height: 100%;
