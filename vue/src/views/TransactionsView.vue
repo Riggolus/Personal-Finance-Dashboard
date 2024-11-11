27ea204e102c959a9b636abd5d58d4bd4944b200
Riggolus <nknappen@gmail.com>
2024-11-10 19:35:28 -0500

    Added buttons in the transaction

--- vue/src/views/TransactionsView.vue
+++ vue/src/views/TransactionsView.vue
@@ -1,7 +1,57 @@
 <template>
-    <h1>This is where the user will log transactions</h1>
+    <div id="main">
+        <div id="expense">
+            <button @click="showExpenseForm">Log Expense</button>
+            <form action="submit" v-if="expenseForm">
+                <h3>test expense</h3>
+            </form>
+        </div>
+        <div id="earnings">
+            <button @click="showEarningsForm">Log Earnings</button>
+            <form action="submit" v-if="earningsForm">
+                <h3>test earnings</h3>
+            </form>
+
+        </div>
+    </div>
 </template>
 
-<script></script>
+<script>
+export default {
+    data() {
+        return {
+            expenseForm: false,
+            earningsForm: false
+        }
+    },
+    methods: {
+        showExpenseForm() {
+            this.expenseForm = true;
+            this.earningsForm = false;
+        },
+        showEarningsForm() {
+            this.earningsForm = true;
+            this.expenseForm = false;
+        }
+    }
+}
+</script>
 
-<style></style>
+<style scoped>
+#main {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 20px;
+    padding: 20px;
+}
+
+
+button {
+    background-color: #007bff;
+    color: white;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+}
+</style>
