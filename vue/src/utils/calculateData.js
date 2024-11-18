9ba5edb9813ac31ecebc90598182815bf0874faa
Riggolus <nknappen@gmail.com>
2024-11-18 11:04:55 -0500

    Improved util/calculateData functionality

--- vue/src/utils/calculateData.js
+++ vue/src/utils/calculateData.js
@@ -1,13 +1,24 @@
 import TransactionsService from "../services/TransactionsService";
+import BudgetsService from "../services/BudgetsService";
 
 export const calculateData = async () => {
 
     try {
         const response = await TransactionsService.getTransactions(); // Wait for the API call
+        // Gonna get teh data for budgets
+        const responseBudget = await BudgetsService.getBudgets();
+
         const transactions = response.data;
+        const budgets = responseBudget.data;
 
         let totalIncome = 0;
         let totalExpense = 0;
+
+        let housingTransactions = [];
+        let transportationTransactions = [];
+        let foodTransactions = [];
+        let Entertainment = [];
+        let otherTrasactions = [];
 
         // Process the transactions
         transactions.forEach(transaction => {
@@ -16,12 +27,28 @@ export const calculateData = async () => {
             } else {
                 totalExpense += transaction.amount;
             }
+            // Categorizing expence by category
+            if (transaction.category === 'Housing') {
+                housingTransactions.push(transaction);
+            } else if (transaction.category === 'Transportation') {
+                transportationTransactions.push(transaction);
+            } else if (transaction.category === 'Food') {
+                foodTransactions.push(transaction);
+            } else if (transaction.category === 'Entertainment') {
+                Entertainment.push(transaction);
+            } else {
+                otherTrasactions.push(transaction);
+            }
         });
+
+        const totalBudget = budgets.reduce((acc, budget) => acc + budget.amountLimit, 0);
 
         const totalAmount = totalIncome - totalExpense;
 
         // Return the calculated data
-        return { totalIncome, totalExpense, totalAmount };
+        return { totalIncome, totalExpense, totalAmount, totalBudget, 
+            housingTransactions, transportationTransactions, foodTransactions, 
+            Entertainment, otherTrasactions };
     } catch (error) {
         console.error("Error fetching transactions:", error);
         return { totalIncome: 0, totalExpense: 0, totalAmount: 0 };
