f8717257c0a227dba5bc9879bfd9990e147d7fd8
Riggolus <nknappen@gmail.com>
2024-12-09 15:16:13 -0500

    Created AI Chatbot

--- vue/src/components/AssistantComponent.vue
+++ vue/src/components/AssistantComponent.vue
@@ -1,13 +1,15 @@
 <template>
     <div id="assistant">
-        <h3>Testing Assistant</h3>
-        <form action="">
-            <label for="assistantInput">Enter a command:</label>
-            <input type="text" id="assistantInput" name="assistantInput">
-            <button @click.prevent="getResponse">Submit</button>
+        <h3>Assistant powered by Gemini</h3>
+        <form @submit.prevent="getResponse">
+            <label for="assistantInput">Enter a question:</label>
+            <input type="text" id="assistantInput" v-model="assistantInput" name="assistantInput">
+            <button type="submit">Submit</button>
         </form>
         <div id="assistantOutput">
-            <p>Assistant output will go here</p>
+            <div id="conversation" v-for="message in conversation" :key="message.text">
+                <div><strong>{{ message.role }}:</strong> {{ message.text }}</div>
+            </div>
         </div>
     </div>
 </template>
@@ -19,17 +21,25 @@ export default {
     data() {
         return {
             assistantInput: "",
-            assistantOutput: ""
-        }
+            assistantOutput: "",
+            conversation: []
+        };
     },
     methods: {
         getResponse() {
+            console.log(this.assistantInput);
+
+            this.conversation.push({ role: 'user', text: this.assistantInput });
+
             GoogleGeminiService.postGeminiMessage(this.assistantInput)
                 .then(response => {
-                    this.assistantOutput = response.data;
+                    this.assistantOutput = response.data; 
+                    console.log(response.data);
+
+                    this.conversation.push({ role: 'assistant', text: this.assistantOutput.answer.replace(/^A: /, '') });
                 })
                 .catch(e => {
-                    console.log(e);
+                    console.log(e); 
                 });
         }
     }
@@ -37,4 +47,28 @@ export default {
 </script>
 
 <style>
+#assistant {
+    display: flex;
+    flex-direction: column;
+}
+
+#assistant form {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+}
+
+#assistantInput {
+    width: 100%;
+}
+
+#assistantOutput {
+    margin-top: 1rem;
+    border-top: 1px solid #ccc;
+    padding-top: 1rem;
+}
+
+#conversation {
+    margin-bottom: 1rem;
+}
 </style>
